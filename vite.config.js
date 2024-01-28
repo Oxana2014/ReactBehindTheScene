382187b0919e114d29e5e2b6058c5c9868414a62
Oksana Kormilina <keysb@mail.ru>
2024-01-28 20:20:59 +0400

    Add MillionJS for speed up the application

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import million from "million/compiler";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    million.vite({
+      auto:  {
+        threshold: 0.05,
+        skip: ["useBadHook", /badVariable/g],
+      },
+    }),
+    react()],
 })
