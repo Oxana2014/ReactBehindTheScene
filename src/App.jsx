cb3b3b1eeba111a17919dcedc6ca259e61c61d66
Oksana Kormilina <keysb@mail.ru>
2024-01-28 16:31:54 +0400

    Key schould not be an index of array

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
       <main>
       <ConfigureCounter onSet={handleSetCount} />
         <Counter initialCount={chosenCount} />
+        <Counter initialCount={0} />
       </main>
     </>
   );
